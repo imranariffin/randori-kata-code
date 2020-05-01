d1200eafe4e6d26f51334622e437fe41300413f9
Imran Ariffin <ariffin.imran@gmail.com>
2020-04-30 22:04:44 -0400

    Setup unit test for empty root endpoint

---
+++ tests/routes/index.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+
+const routes = require('app/routes')
+
+describe('api index', () => {
+  test('root endpoint response', async () => {
+    const response =  await request(routes)
+      .get('/')
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({})
+  })
+})
+
