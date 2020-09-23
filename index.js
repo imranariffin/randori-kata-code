b9129004e78977ec1692e34c672d3bf21978b418
Imran Ariffin <ariffin.imran@gmail.com>
2020-09-23 19:23:27 -0400

    Init code-chat connection with backend

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+const cors = require('cors')
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+// })
+
+corsOptions = {
+  origin: 'http://localhost:8080',
+  // methods: ['GET']
+}
+
+const corsCallBack = (req, cb) => {
+  console.log(`Request: URL=${req.url}; method=${req.method}; headers=${JSON.stringify(req.headers)};`)
+  cb(null, corsOptions)
+}
+
+app.use(cors(corsCallBack))
+
+app.options('/test/cors', cors(corsCallBack))
+app.del('/test/cors', cors(corsCallBack), (req, res) => {
+  console.log(`Request: URL=${req.url}; method=${req.method}; headers=${JSON.stringify(req.headers)};`)
+  res.send('zzz')
 })
 
 io.on('connection', (socket) => {
