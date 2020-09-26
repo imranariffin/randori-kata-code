5a50ddecfd34fd798ca25e403ead5adb86c92d6e
Imran Ariffin <ariffin.imran@gmail.com>
2020-09-26 18:25:29 -0400

    Setup dev digital ocean in dev workflow
    Setup Dev-PR workflow

---
+++ scripts/do-create-droplet-if-not-exists.sh
@@ -0,0 +1,28 @@
+echo "Environment=$RANDORIKATA__ENV"
+
+ENV=$RANDORIKATA__ENV
+DROPLET_NAME=$RANDORIKATA__CODE__APP_NAME--$RANDORIKATA__ENV
+DROPLET_SIZE=$RANDORIKATA__CODE__DROPLET_SIZE
+
+echo "Checking if droplet '$DROPLET_NAME' already exists"
+EXISTS=`doctl compute droplet list \
+ --format Name \
+ --no-header \
+ | grep $DROPLET_NAME \
+ | uniq`
+
+if [ -z "$EXISTS" ]
+then
+  echo "Droplet '$DROPLET_NAME' not created. Creating"
+  doctl compute droplet create \
+   --image ubuntu-18-04-x64 \
+   --size $DROPLET_SIZE \
+   --region tor1 \
+   --ssh-keys 67:65:5f:d9:e6:a0:f0:e5:a4:38:9a:df:b9:c7:5d:27 \
+   --tag-name $ENV \
+   --wait \
+   -v \
+   $DROPLET_NAME
+else
+  echo "Droplet '$DROPLET_NAME' already created. Exiting"
+fi
---
+++ scripts/do-create-project-if-not-exists.sh
@@ -0,0 +1,19 @@
+echo "Environment=$RANDORIKATA__ENV"
+
+PROJECT_NAME=$RANDORIKATA__APP_NAME--$RANDORIKATA__ENV
+EXISTS=`doctl projects list \
+  --format Name \
+  --no-header \
+  | grep $PROJECT_NAME \
+  | uniq`
+
+echo "Checking if project '$PROJECT_NAME' already exists"
+if [ -z "$EXISTS" ]
+then
+  echo "Project '$PROJECT_NAME' not created. Creating"
+  doctl projects create \
+   --name $PROJECT_NAME \
+   --purpose "Manage real-time code-sharing"
+else
+  echo "Project '$PROJECT_NAME' already created. Exiting"
+fi
