5e30e80865726da064aee192730e1d4b480f4403
Imran Ariffin <ariffin.imran@gmail.com>
2020-09-26 22:44:13 -0400

    Fix grep in workflow (#10)
    * Fix grep
    
    * Fix indentation

--- scripts/do-create-droplet-if-not-exists.sh
+++ scripts/do-create-droplet-if-not-exists.sh
@@ -6,23 +6,23 @@ DROPLET_SIZE=$RANDORIKATA__CODE__DROPLET_SIZE
 
 echo "Checking if droplet '$DROPLET_NAME' already exists"
 EXISTS=`doctl compute droplet list \
- --format Name \
- --no-header \
- | grep $DROPLET_NAME \
- | uniq`
+  --format Name \
+  --no-header \
+  | grep "^$DROPLET_NAME$" \
+  | uniq`
 
 if [ -z "$EXISTS" ]
 then
   echo "Droplet '$DROPLET_NAME' not created. Creating"
   doctl compute droplet create \
-   --image ubuntu-18-04-x64 \
-   --size $DROPLET_SIZE \
-   --region tor1 \
-   --ssh-keys 67:65:5f:d9:e6:a0:f0:e5:a4:38:9a:df:b9:c7:5d:27 \
-   --tag-name $ENV \
-   --wait \
-   -v \
-   $DROPLET_NAME
+    --image ubuntu-18-04-x64 \
+    --size $DROPLET_SIZE \
+    --region tor1 \
+    --ssh-keys 67:65:5f:d9:e6:a0:f0:e5:a4:38:9a:df:b9:c7:5d:27 \
+    --tag-name $ENV \
+    --wait \
+    -v \
+    $DROPLET_NAME
 else
   echo "Droplet '$DROPLET_NAME' already created. Exiting"
 fi
--- scripts/do-create-project-if-not-exists.sh
+++ scripts/do-create-project-if-not-exists.sh
@@ -4,7 +4,7 @@ PROJECT_NAME=$RANDORIKATA__APP_NAME--$RANDORIKATA__ENV
 EXISTS=`doctl projects list \
   --format Name \
   --no-header \
-  | grep $PROJECT_NAME \
+  | grep "^$PROJECT_NAME$" \
   | uniq`
 
 echo "Checking if project '$PROJECT_NAME' already exists"
@@ -12,8 +12,8 @@ if [ -z "$EXISTS" ]
 then
   echo "Project '$PROJECT_NAME' not created. Creating"
   doctl projects create \
-   --name $PROJECT_NAME \
-   --purpose "Manage real-time code-sharing"
+    --name $PROJECT_NAME \
+    --purpose "Manage real-time code-sharing"
 else
   echo "Project '$PROJECT_NAME' already created. Exiting"
 fi
